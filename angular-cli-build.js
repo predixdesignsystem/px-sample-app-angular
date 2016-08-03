707cc1c226bfffd23dbbbdef01d3e45fbe44dd58
Alex <alex.leites@ge.com>
2016-08-02 18:19:54 -0700

    Add SASS support

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -8,6 +8,9 @@ var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
 module.exports = function(defaults) {
   return new Angular2App(defaults, {
+    sassCompiler: {  // all options here: https://github.com/sass/node-sass
+      includePaths: [ 'public/bower_components' ]  
+    },    
     vendorNpmFiles: [
       'systemjs/dist/system-polyfills.js',
       'systemjs/dist/system.src.js',
