af0439cdb26747ff6fbed2b17ee47924bd7e9883
Alex <alex.leites@ge.com>
2017-01-12 14:54:27 -0800

    chore(build): work around bootstrap detection bug

--- src/main-polymer.ts
+++ src/main-polymer.ts
@@ -2,3 +2,13 @@
 document.addEventListener('WebComponentsReady', () => {
   require('./main.ts');
 });
+
+// https://github.com/angular/angular-cli/issues/3540#issuecomment-270627460
+// HACK: horrible workaround for AoT bootstrap detection bug
+import './polyfills.ts';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+let hackThis = false; 
+if (hackThis) {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+}
